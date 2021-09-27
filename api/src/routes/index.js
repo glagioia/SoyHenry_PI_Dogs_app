f844975570579ef95c3218d8b2df0037fd7084d1
Yglagioia <gonzalolagioia@gmail.com>
2021-09-27 13:44:53 -0300

    back listo

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,20 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const dogs = require('./dogs');
+const dog = require('./dog');
+const idRaza = require('./idRaza');
+const temperament = require('./temperament');
+const temperamentIntermediate = require('./temperamentIntermediate');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use('/dogs', dogs);
+router.use('/dog', dog);
+router.use('/idRaza', idRaza);
+router.use('/temperament', temperament);
+router.use('/intermediate', temperamentIntermediate);
 
 module.exports = router;
